80790e30c0df0442352e2f61682d4008eb5d612b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-23 09:48:57 -0300

    projeto MS-Pedido

---
+++ ms-pedido/src/main/java/com/github/Atu141/ms_pedido/entities/Status.java
@@ -0,0 +1,11 @@
+package com.github.Atu141.ms_pedido.entities;
+
+public enum Status {
+    REALIZADO,
+    AGUARDANDO_PAGAMENTO,
+    PAGO,
+    CONFIRMADO,
+    PRONTO_PARA_ENTREGA,
+    SAIU_PARA_ENTREGA,
+    CANCELADO
+}
---
+++ ms-pedido/src/test/java/com/github/Atu141/ms_pedido/MsPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Atu141.ms_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
